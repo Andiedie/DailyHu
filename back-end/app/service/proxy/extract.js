e761b33e39b3b6fd3a203fadb93fd9449b3abaaa
Zhu Chu <mystery490815101@gmail.com>
2017-05-12 10:36:15 +0800

    注释、增加屏蔽link的接口

--- back-end/app/service/proxy/extract.js
+++ back-end/app/service/proxy/extract.js
@@ -15,8 +15,14 @@ const extract = async (url, selector) => {
 
   $('script, link').remove();
 
+  // 基于腾讯云对象存储加速的基础css
   $('head').append('<link rel="stylesheet" href="http://midterm-1252605895.cosgz.myqcloud.com/modest.css"/>');
+
+  // 绕过防盗链
   $('head').append('<meta name="referrer" content="never">');
+
+  // 屏蔽页面跳转
+  // $('a').removeAttr('href').removeAttr('target').removeAttr('rel');
 
   return $;
 };
